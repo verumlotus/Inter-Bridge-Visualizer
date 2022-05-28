3df343c9be84183a09ec4206d47d9f198b491211
verumlotus <universumtremendum@gmail.com>
2022-05-28 17:30:16 -0400

    Func to get all bridge data

--- backend/src/scripts/dataCollection.ts
+++ backend/src/scripts/dataCollection.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import * as bridges from "../constants/bridges.json";
+import * as bridgeJson from "../constants/bridges.json";
 import { defiLlamaProtocolEndpoint } from './../constants/endpoints';
 
 /**
@@ -9,8 +9,21 @@ import { defiLlamaProtocolEndpoint } from './../constants/endpoints';
 async function fetchBridgeData(bridge: string) {
     const url = defiLlamaProtocolEndpoint + bridge;
     const response = await axios.get(url);
-    console.log(response);
     return response.data;
 }
 
-fetchBridgeData("stargate");
+async function fetchAllBridgeData(bridges: string[]) {
+    let bridgeData = {};
+    const _data = await Promise.all(bridges.map(fetchBridgeData))
+    // Format and clean data a bit 
+    for (const obj of _data) {
+        if ('statusCode' in obj && obj.statusCode != 200) {
+            // TODO: Sentry logging here
+        } else {
+            bridgeData[obj.name] = obj;
+        }
+    }
+    return bridgeData;
+}
+
+fetchAllBridgeData(bridgeJson.bridges);
