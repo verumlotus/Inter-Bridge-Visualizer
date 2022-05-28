eb4f968b090caf2ec375790619d2849e51fc9956
verumlotus <universumtremendum@gmail.com>
2022-05-28 17:03:14 -0400

    more scaffolding

---
+++ backend/src/constants/endpoints.ts
@@ -0,0 +1,1 @@
+export const defiLlamaProtocolEndpoint = "https://api.llama.fi/protocol/";
--- backend/src/scripts/dataCollection.ts
+++ backend/src/scripts/dataCollection.ts
@@ -1,2 +1,16 @@
-import "axios";
+import axios from "axios";
+import * as bridges from "../constants/bridges.json";
+import { defiLlamaProtocolEndpoint } from './../constants/endpoints';
 
+/**
+ * @notice fetches the data for a given bridge
+ * @param bridge 
+ */
+async function fetchBridgeData(bridge: string) {
+    const url = defiLlamaProtocolEndpoint + bridge;
+    const response = await axios.get(url);
+    console.log(response);
+    return response.data;
+}
+
+fetchBridgeData("stargate");
