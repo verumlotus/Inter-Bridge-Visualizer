dc5dbeb16c24c7be3f92acf811d11aa075096d3f
verumlotus <universumtremendum@gmail.com>
2022-05-31 09:57:33 -0400

    endpoints

--- backend/src/utils/dbQuery.ts
+++ backend/src/utils/dbQuery.ts
@@ -8,3 +8,35 @@ export async function dbGetTvlByBridge() {
 export async function dbGetTvlByChain() {
     return await prisma.tvlByChain.findMany();
 }
+
+export async function dbGetChainTvlByBridge(chain: string) {
+    return await prisma.tvlSingleChainByBridge.findFirst({
+        where: {
+            chain: chain
+        }
+    })
+}
+
+export async function dbGetChainTvlByAsset(chain: string) {
+    return await prisma.tvlSingleChainByAsset.findFirst({
+        where: {
+            chain: chain
+        }
+    })
+}
+
+export async function dbGetAssetTvlByBridge(asset: string) {
+    return await prisma.tvlSingleAssetByBridge.findFirst({
+        where: {
+            asset: asset
+        }
+    })
+}
+
+export async function dbGetAssetTvlByChain(asset: string) {
+    return await prisma.tvlSingleAssetByChain.findFirst({
+        where: {
+            asset: asset
+        }
+    })
+}
