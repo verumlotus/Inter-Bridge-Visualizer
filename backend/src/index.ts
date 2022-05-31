23db74aa7bfdab8c17f98e12c34ca0830e0074de
verumlotus <universumtremendum@gmail.com>
2022-05-31 09:45:07 -0400

    Endpoints for tvlByBridge & tvlByChain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,24 @@
 import express, { Express, Request, Response} from 'express'
+import { dbGetTvlByBridge, dbGetTvlByChain} from "./utils/dbQuery"
 const app: Express = express()
 const PORT = 3000
 
+// Dummy route for testing
 app.get('/', (req, res) => {
     res.send("Hello there mate!")
 })
 
 // 1. TvlByBridge
 app.get('/tvlByBridge', async (req, res) => {
+    res.send(await dbGetTvlByBridge())
+})
 
+// 2. TvlByChain
+app.get('/tvlByChain', async (req, res) => {
+    res.send(await dbGetTvlByChain())
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`)
 })
 
--- backend/src/utils/dbQuery.ts
+++ backend/src/utils/dbQuery.ts
@@ -1,2 +1,10 @@
 import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
+export async function dbGetTvlByBridge() {
+    return await prisma.tvlByBridge.findMany();
+}
+
+export async function dbGetTvlByChain() {
+    return await prisma.tvlByChain.findMany();
+}
