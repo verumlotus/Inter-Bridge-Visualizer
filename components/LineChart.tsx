b89225c623b7f3c97829ca06ecaef5cafb97dbd9
verumlotus <universumtremendum@gmail.com>
2022-06-01 13:39:03 -0400

    Factor Line chart out into component

---
+++ components/LineChart.tsx
@@ -0,0 +1,79 @@
+import {ResponsiveLine} from "@nivo/line"
+
+export default function LineChart(props: {data: any}){
+    let {data = null} = props;
+    return (
+        <ResponsiveLine 
+            data={data}
+            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+            xScale={{ 
+                type: 'time',
+                format: '%s',
+            }}
+            yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: false,
+                reverse: false
+            }}
+            yFormat={">.2~g"}
+            colors={{ scheme: "category10" }}
+            enableArea={true}
+            enablePoints={false}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                format: function(value: Date){ 
+                    return `${value.toLocaleString('default', { month: 'long' }).slice(0,3)} ${value.getFullYear()}`
+                },
+                tickValues: 2,
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'Time',
+                legendOffset: 36,
+                legendPosition: 'middle'
+            }}
+            axisLeft={{
+                format: function(value: number) {
+                    return Math.round(value / 1e6)
+                },
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'USD (Million)',
+                legendOffset: -50,
+                legendPosition: 'middle'
+            }}
+            enableSlices={"x"}
+            isInteractive={true}
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 100,
+                    translateY: 0,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemBackground: 'rgba(0, 0, 0, .03)',
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+            ]}   
+        />
+    );
+}
--- pages/TvlByBridge.tsx
+++ pages/TvlByBridge.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import useSWR from "swr";
-import {ResponsiveLine} from "@nivo/line"
-import moment from "moment"
+import LineChart from "../components/LineChart";
 
 async function fetcher(url: string) {
     const response = await axios.get(url);
@@ -31,79 +30,8 @@ export default function TvlByBridge() {
             <p>
                 Explanation of what questions this can answer go here!
             </p>
-            {!error && data && (
-                <ResponsiveLine 
-                    data={data}
-                    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                    xScale={{ 
-                        type: 'time',
-                        format: '%s',
-                    }}
-                    yScale={{
-                        type: 'linear',
-                        min: 'auto',
-                        max: 'auto',
-                        stacked: false,
-                        reverse: false
-                    }}
-                    yFormat={">.2~g"}
-                    colors={{ scheme: "category10" }}
-                    enableArea={true}
-                    enablePoints={false}
-                    axisTop={null}
-                    axisRight={null}
-                    axisBottom={{
-                        format: function(value: Date){ 
-                            return `${value.toLocaleString('default', { month: 'long' }).slice(0,3)} ${value.getFullYear()}`
-                        },
-                        tickValues: 2,
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'Time',
-                        legendOffset: 36,
-                        legendPosition: 'middle'
-                    }}
-                    axisLeft={{
-                        format: function(value: number) {
-                            return Math.round(value / 1e6)
-                        },
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'USD (Million)',
-                        legendOffset: -50,
-                        legendPosition: 'middle'
-                    }}
-                    enableSlices={"x"}
-                    isInteractive={true}
-                    legends={[
-                        {
-                            anchor: 'bottom-right',
-                            direction: 'column',
-                            justify: false,
-                            translateX: 100,
-                            translateY: 0,
-                            itemsSpacing: 0,
-                            itemDirection: 'left-to-right',
-                            itemWidth: 80,
-                            itemHeight: 20,
-                            itemOpacity: 0.75,
-                            symbolSize: 12,
-                            symbolShape: 'circle',
-                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                            effects: [
-                                {
-                                    on: 'hover',
-                                    style: {
-                                        itemBackground: 'rgba(0, 0, 0, .03)',
-                                        itemOpacity: 1
-                                    }
-                                }
-                            ]
-                        }
-                    ]}   
-                />
+            {!error && data &&(
+                <LineChart data={data}/>
             )}
         </div>
     )
