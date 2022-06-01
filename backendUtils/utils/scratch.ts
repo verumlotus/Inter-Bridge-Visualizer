913c5cb831a54155d332c40feb7e83bbcb5d837d
verumlotus <universumtremendum@gmail.com>
2022-06-01 16:40:22 -0400

    Done with nivo charts

--- backendUtils/utils/scratch.ts
+++ backendUtils/utils/scratch.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function temp() {
-    const data = await prisma.tvlSingleChainByBridge.findMany()
+    const data = await prisma.tvlSingleAssetByBridge.findMany()
     let result = []
     for (const infoObj of data) {
-        result.push(infoObj.chain)
+        result.push(infoObj.asset)
     }
     console.log(JSON.stringify(result));
     
---
+++ pages/TvlSingleAssetByBridge.tsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import * as assetsJson from "../backendUtils/constants/assets.json";
+import LineChart from "../components/LineChart";
+import SearchBar from "../components/SearchBar";
+
+const assets = assetsJson["assets"]
+
+export default function TvlSingleAssetByBridge() {
+    let [asset, setAsset] = useState("USDC")
+    let [data, setData] = useState([]);
+
+    const URL_PREFIX = "/api/assetTvlByBridge";
+    useEffect(() => {
+        async function fetcher(urlPrefix: string) {
+            let url = `${urlPrefix}/${asset}`
+            const response = await axios.get(url);
+            // Sort to filter only by top 12 assets
+            const result = response.data['bridgeTvlData'].sort((a, b) => {
+                return Number(a['data'].at(-1)['y']) > Number(b['data'].at(-1)['y'])
+            }).slice(0, 15)
+            setData(result);
+        }
+        if (asset) {
+            fetcher(URL_PREFIX);
+        }
+    }, [asset])
+
+    function onSearchSubmit(value: string) {
+        if (value) {
+            setAsset(value);
+        }
+    }
+
+    return (
+        <div style={{height: "65vh", width: "90vw"}}>
+            <h2>
+                Single Asset TVL By Bridge. Current data is for {asset}
+            </h2>
+            <p>
+                Explanation of what questions this can answer go here!
+            </p>
+            <SearchBar
+                options={assets}
+                onSubmitFn={onSearchSubmit}
+            />
+            {data &&(
+                <LineChart data={data}/>
+            )}
+        </div>
+    )
+}
